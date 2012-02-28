fa2ca2cbcfb892239866fb466cc06a4a56eb93ed
onami <onaixt@gmail.com>
2012-02-28 16:49:17 +0600

    2 attempt

---
+++ experiments/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    public void Test(int test1, int i) { }
+    static void Main()
+    {
+
+        int? t = 5, d;
+        d = t;
+
+        Console.WriteLine("{0}", d);
+
+        Console.ReadKey();
+
+
+    } 
+}
